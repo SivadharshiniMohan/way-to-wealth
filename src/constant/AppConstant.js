f00df9a05a3853cc34887aeecfc67bcd736ef1a4
SivadharshiniMohan <sivadharshini25001@gmail.com>
2024-05-15 20:12:54 +0530

    Updated date and link on jun

--- src/constant/AppConstant.js
+++ src/constant/AppConstant.js
@@ -1,3 +1,3 @@
-export const DATE = "05/05/2024, Sunday";
+export const DATE = "02/06/2024, Sunday";
 export const TIME = "11:00 AM - 2:00 PM IST";
-export const PAYMENT = "https://waytowealth.rpy.club/wdp/bd7Iu8hmvS"
+export const PAYMENT = "https://rpy.club/webinar/c5UPtEdTV2"
