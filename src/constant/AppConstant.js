1f1be5e01aff40e5802005691727415170c939b0
SivadharshiniMohan <sivadharshini25001@gmail.com>
2024-06-17 14:48:15 +0530

    Aug date and like changed

--- src/constant/AppConstant.js
+++ src/constant/AppConstant.js
@@ -1,3 +1,3 @@
-export const DATE = "02/06/2024, Sunday";
+export const DATE = "04/08/2024, Sunday";
 export const TIME = "11:00 AM - 2:00 PM IST";
-export const PAYMENT = "https://rpy.club/webinar/c5UPtEdTV2"
+export const PAYMENT = "https://rigi.club/webinar/nVU1FY8hSi"
