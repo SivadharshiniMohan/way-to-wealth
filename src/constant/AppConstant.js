babdd04cf35b53355392f1f3e0830b4be6b5dcc9
SivadharshiniMohan <sivadharshini25001@gmail.com>
2024-10-18 21:42:28 +0530

    NOV change

--- src/constant/AppConstant.js
+++ src/constant/AppConstant.js
@@ -1,3 +1,3 @@
-export const DATE = "06/10/2024, Sunday";
+export const DATE = "03/11/2024, Sunday";
 export const TIME = "11:00 AM - 2:00 PM IST";
 export const PAYMENT = "https://rpy.club/webinar/LTB4RiZVET"
